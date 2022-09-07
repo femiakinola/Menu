91ffe5081f7b1df6dec0f72788b1ca34189ad302
femiakinola <akinolapaulo1@gmail.com>
2022-09-07 15:56:50 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  // const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  // const filterItems = (category) => {
+  // const newItems = items.filter((item) => item.category === category);
+  // setMenuItems(newItems);
+  // };
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+  const fetchCategories = async () => {
+    const response = await axios.get(
+      "https://www.themealdb.com/api/json/v1/1/categories.php"
+    );
+    console.log(response);
+    setCategories(response.data.categories);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="App">
+        <h1>Menu</h1>
+        {/* <header className="App-header"></header> */}
+        {categories.map((item) => {
+          return (
+            <article key={item.idCategory} className="menu-item">
+              <img
+                src={item.strCategoryThumb}
+                alt={item.strCategory}
+                className="photo"
+              />
+              <div className="item-info">
+                <header>
+                  <h4>{item.strCategory}</h4>
+                </header>
+                <p className="item-text">{item.strCategoryDescription}</p>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+
+      {/* <Menu /> */}
+      {/* <Categories /> */}
+    </main>
   );
 }
 
